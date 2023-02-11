d9d41af3930cbf35fc6b838455188a268c09d819
LeidyLozano <l.lozanof@uniandes.edu.co>
2023-02-10 23:15:51 -0500

    xd

--- console.py
+++ console.py
@@ -32,6 +32,15 @@ def EVALUARCODIGO ():
     rta = controller.parse_code(texto)
 
     print(rta)
+
+    if rta == None:
+        print("="*50)  
+        print("El codigo no sirve")
+        print("="*50)
+    else:
+        print("="*50)  
+        print("¡El codigo sí sirve!")
+        print("="*50)
     #TODO Se comento el return texto, ya que no se sentía pertinente. 
     #return texto
     
